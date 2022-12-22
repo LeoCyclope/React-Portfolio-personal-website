45eb80ef32c6baa8ed4700198072416becd29912
LeoCyclope <72453623+LeoCyclope@users.noreply.github.com>
2022-12-22 15:53:52 +0100

    Update with Google A and First part about me

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-465B1M5Z5X"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
+ReactGA.send("pageview");
 
 function App() {
+
    return (
       <div>
          <BrowserRouter>
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -54,8 +54,12 @@ export default function About() {
     return (
         <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'3rem'}>
             <Terminal text={aboutMeText()}/>
-            <Terminal text={skillsText()}/>
-            <Terminal text={miscText()}/>
+
         </Box>
     )
 }
+
+/*
+            <Terminal text={skillsText()}/>
+            <Terminal text={miscText()}/>
+            */
